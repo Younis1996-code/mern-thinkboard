7f347f71bb9b48c586fd091fae0db88191678bba
Younis1996-code <younisissa205@gmail.com>
2025-11-08 00:37:59 +0300

    second commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,16 +3,22 @@ import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import ratelimiter from "./middleware/rateLimiter.js";
-import cors from "cors"
+import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
-app.use(cors({
-  origin: 'http://localhost:5173'
-}))
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
 
 app.use(express.json());
 
@@ -25,10 +31,16 @@ app.use(ratelimiter);
 
 app.use("/api/notes", notesRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get((req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 connectDB().then(() => {
-    app.listen(PORT, () => {
-      console.log("Server running on port:", PORT);
-    });
-
+  app.listen(PORT, () => {
+    console.log("Server running on port:", PORT);
+  });
 });
-
